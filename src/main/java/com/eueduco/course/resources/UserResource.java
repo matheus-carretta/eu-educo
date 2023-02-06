b35dbae975c528674f27848cd9610f09e485be1f
matheus-carretta <matheuscarretta@gmail.com>
2023-02-06 17:12:57 -0300

    feat: user entity and resource created

---
+++ src/main/java/com/eueduco/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.eueduco.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eueduco.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Matheus", "m@gmail.com", "27995121922", "coxinha123");
+		return ResponseEntity.ok().body(u);
+	}
+}
