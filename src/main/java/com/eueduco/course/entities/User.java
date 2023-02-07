945182fe14b13e0100cb820db38992040466f11e
matheus-carretta <matheuscarretta@gmail.com>
2023-02-06 23:18:05 -0300

    feat: service layer and method findById

--- src/main/java/com/eueduco/course/entities/User.java
+++ src/main/java/com/eueduco/course/entities/User.java
@@ -1,8 +1,6 @@
 package com.eueduco.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,9 +10,8 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,7 +19,8 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
+
+
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -72,9 +70,13 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -86,8 +88,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 }
