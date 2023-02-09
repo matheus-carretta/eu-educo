a3922829c0a3544b4e9c3efa1795a143945b517d
matheus-carretta <matheuscarretta@gmail.com>
2023-02-09 16:33:04 -0300

    feat: create category route

---
+++ src/main/java/com/eueduco/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.eueduco.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eueduco.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
