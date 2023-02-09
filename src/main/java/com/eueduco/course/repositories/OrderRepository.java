53a9bfe3ea1b3bf91d6c1f8d709f71045837e60c
matheus-carretta <matheuscarretta@gmail.com>
2023-02-09 14:56:36 -0300

    feat: create orders route

---
+++ src/main/java/com/eueduco/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.eueduco.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eueduco.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
